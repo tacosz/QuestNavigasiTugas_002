ba2c4e63fd8493968d585f714b96e4cda4a279af
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-08 16:48:25 +0700

    membuat box berisi column untuk player data list

--- app/src/main/java/com/example/praktikum6pam/view/list.kt
+++ app/src/main/java/com/example/praktikum6pam/view/list.kt
@@ -1,8 +1,31 @@
 package com.example.praktikum6pam.view
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.praktikum6pam.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -85,6 +108,15 @@ fun ListPemain(
             "POSISI" to "Forward",
             "ASAL" to "France"
         ),
-
     )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 100.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Player Data List")
+        }
+    }
 }
