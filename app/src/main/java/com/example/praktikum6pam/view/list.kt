a1f558917b1152de70d38a04510234f9f52066c4
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-08 16:24:57 +0700

    membuat list of berisi data inputan

--- app/src/main/java/com/example/praktikum6pam/view/list.kt
+++ app/src/main/java/com/example/praktikum6pam/view/list.kt
@@ -12,5 +12,79 @@ fun ListPemain(
     onDaftar: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    val allBiodata = listOf(
+        listOf(
+            "NAMA LENGKAP" to "Xabi Alonso",
+            "NUMBER" to "-",
+            "POSISI" to "Head Coach",
+            "ASAL" to "Spain"
+        ),
+        listOf(
+            "NAMA LENGKAP" to "Courtois",
+            "NUMBER" to "1",
+            "POSISI" to "Goalkeeper",
+            "ASAL" to "Belgium"
+        ),
+        listOf(
+            "NAMA LENGKAP" to "E. Militao",
+            "NUMBER" to "3",
+            "POSISI" to "Defender",
+            "ASAL" to "Brazil"
+        ),
+        listOf(
+            "NAMA LENGKAP" to "A. Carreras",
+            "NUMBER" to "18",
+            "POSISI" to "Defender",
+            "ASAL" to "Spain"
+        ),
+        listOf(
+            "NAMA LENGKAP" to "Huijsen",
+            "NUMBER" to "24",
+            "POSISI" to "Defender",
+            "ASAL" to "Spain"
+        ),
+        listOf(
+            "NAMA LENGKAP" to "Bellingham",
+            "NUMBER" to "5",
+            "POSISI" to "Midfielder",
+            "ASAL" to "England"
+        ),
+        listOf(
+            "NAMA LENGKAP" to "Camavinga",
+            "NUMBER" to "6",
+            "POSISI" to "Midfielder",
+            "ASAL" to "France"
+        ),
+        listOf(
+            "NAMA LENGKAP" to "Valverde",
+            "NUMBER" to "8",
+            "POSISI" to "Midfielder",
+            "ASAL" to "Uruguay"
+        ),
+        listOf(
+            "NAMA LENGKAP" to "Tchouameni",
+            "NUMBER" to "14",
+            "POSISI" to "Midfielder",
+            "ASAL" to "France"
+        ),
+        listOf(
+            "NAMA LENGKAP" to "Arda Guler",
+            "NUMBER" to "15",
+            "POSISI" to "Midfielder",
+            "ASAL" to "Belgium"
+        ),
+        listOf(
+            "NAMA LENGKAP" to "Vini Jr.",
+            "NUMBER" to "7",
+            "POSISI" to "Forward",
+            "ASAL" to "Brazil"
+        ),
+        listOf(
+            "NAMA LENGKAP" to "Mbappe",
+            "NUMBER" to "10",
+            "POSISI" to "Forward",
+            "ASAL" to "France"
+        ),
 
+    )
 }
