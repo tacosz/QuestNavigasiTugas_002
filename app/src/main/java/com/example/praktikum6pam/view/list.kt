3b0d1eec9238d5386f74d0ea619f024d49040e01
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-08 16:01:54 +0700

    membuat fungsi dan deklarasi untuk button

--- app/src/main/java/com/example/praktikum6pam/view/list.kt
+++ app/src/main/java/com/example/praktikum6pam/view/list.kt
@@ -1,2 +1,16 @@
 package com.example.praktikum6pam.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.praktikum6pam.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListPemain(
+    onUtama: () -> Unit,
+    onDaftar: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
