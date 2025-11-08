54e6069cd4c37d2ed4f6d6f93511a0487acb90ff
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-08 14:24:29 +0700

    menambahkan background pada halaman utama

--- app/src/main/java/com/example/praktikum6pam/MainActivity.kt
+++ app/src/main/java/com/example/praktikum6pam/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktikum6pam.ui.theme.Praktikum6PAMTheme
+import com.example.praktikum6pam.view.HalamanUtama
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ...()
+                    HalamanUtama()
                 }
             }
         }
--- app/src/main/java/com/example/praktikum6pam/view/utama.kt
+++ app/src/main/java/com/example/praktikum6pam/view/utama.kt
@@ -1,2 +1,25 @@
 package com.example.praktikum6pam.view
 
+import androidx.compose.foundation.Image
+import com.example.praktikum6pam.R
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanUtama() {
+    val image = painterResource(id = R.drawable.rma)
+    Surface (modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
