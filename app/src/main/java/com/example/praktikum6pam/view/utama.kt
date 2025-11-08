cca1ae911fd96473fdeea301e2b01888a02bffec
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-08 14:32:28 +0700

    membuat column dan menambahkan text

--- app/src/main/java/com/example/praktikum6pam/view/utama.kt
+++ app/src/main/java/com/example/praktikum6pam/view/utama.kt
@@ -1,14 +1,23 @@
 package com.example.praktikum6pam.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import com.example.praktikum6pam.R
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,5 +30,17 @@ fun HalamanUtama() {
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 140.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Real Madrid",
+                color = Color.LightGray,
+                fontSize = 44.sp,
+                fontWeight = FontWeight.Bold)
+        }
     }
 }
