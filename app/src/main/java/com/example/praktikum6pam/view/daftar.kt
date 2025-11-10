f138c7fdfc3fcaffaa523d3445dde7b796e4efd4
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-10 10:31:41 +0700

    mendeklarasikan untuk buttom dan variable yang digunakan

--- app/src/main/java/com/example/praktikum6pam/view/daftar.kt
+++ app/src/main/java/com/example/praktikum6pam/view/daftar.kt
@@ -1,2 +1,22 @@
 package com.example.praktikum6pam.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.example.praktikum6pam.R
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormPendaftaran(
+    onList: () -> Unit,
+    onSubmit: () -> Unit
+) {
+    var txtName by remember { mutableStateOf("") }
+    var txtNumber by remember { mutableStateOf("") }
+    var txtPosition by remember { mutableStateOf("") }
+    var txtOrigin by remember { mutableStateOf("") }
+
+}
