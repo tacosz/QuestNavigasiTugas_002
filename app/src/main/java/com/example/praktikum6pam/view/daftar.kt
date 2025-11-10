66805ab91eb8d1eb926bde0683c8eea6166d8ad5
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-10 11:03:42 +0700

    membuat dan mendeklarasikan alert dialog setelah user input data

--- app/src/main/java/com/example/praktikum6pam/view/daftar.kt
+++ app/src/main/java/com/example/praktikum6pam/view/daftar.kt
@@ -1,15 +1,28 @@
 package com.example.praktikum6pam.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import com.example.praktikum6pam.R
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,5 +58,26 @@ fun FormPendaftaran(
         txtNumber = ""
         txtPosition = ""
         txtOrigin = ""
+    }
+
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = {showDialog = false},
+            containerColor = Color.DarkGray,
+            icon = { Icon(Icons.Filled.CheckCircle,
+                null,
+                modifier = Modifier.size(64.dp))},
+            title = {
+                Column (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Succed", fontWeight = FontWeight.Bold, fontSize = 24.sp, color = Color.White)
+                    Text(text = "Data saved successfully", fontSize = 16.sp, color = Color.White)
+                }
+            }
+        )
     }
 }
