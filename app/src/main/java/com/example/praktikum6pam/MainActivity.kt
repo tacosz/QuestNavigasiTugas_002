72c0eaa66b8a3efcb91886b69ef5c654d4d8fb8d
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-10 11:23:50 +0700

    menambahkan button untuk showdialog yang sudah terjadi

--- app/src/main/java/com/example/praktikum6pam/MainActivity.kt
+++ app/src/main/java/com/example/praktikum6pam/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktikum6pam.ui.theme.Praktikum6PAMTheme
+import com.example.praktikum6pam.view.FormPendaftaran
 import com.example.praktikum6pam.view.HalamanUtama
 import com.example.praktikum6pam.view.ListPemain
 
@@ -27,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ListPemain(onUtama = {}, onDaftar = {})
+                    FormPendaftaran(onList = {}, onSubmit = {})
                 }
             }
         }
--- app/src/main/java/com/example/praktikum6pam/view/daftar.kt
+++ app/src/main/java/com/example/praktikum6pam/view/daftar.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -99,7 +101,15 @@ fun FormPendaftaran(
                 }
             },
             confirmButton = {
-
+                Button(onClick = {
+                    showDialog = false
+                    clearData()
+                },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp))
+                    {
+                        Text("Ok", fontWeight = FontWeight.Bold, fontSize = 14.sp)
+                }
             }
         )
     }
