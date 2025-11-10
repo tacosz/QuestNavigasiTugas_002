3046c470d2c663d93d0585385d2e518b6b6c8882
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-10 13:17:24 +0700

    membuat navigasi route

---
+++ app/src/main/java/com/example/praktikum6pam/navigasi.kt
@@ -0,0 +1,51 @@
+package com.example.praktikum6pam
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum6pam.view.FormPendaftaran
+import com.example.praktikum6pam.view.HalamanUtama
+import com.example.praktikum6pam.view.ListPemain
+
+enum class Navigasi {
+    Main,
+    List,
+    Form
+}
+
+@Composable
+fun App(
+    navController: NavHostController = rememberNavController()
+){
+    Box() {
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Main.name,
+            modifier = Modifier.fillMaxSize()
+        ){
+            composable(route = Navigasi.Main.name) {
+                HalamanUtama(
+                    onBackBtnClick = {navController.navigate(Navigasi.Main.name)}
+                )
+            }
+            composable(route = Navigasi.List.name) {
+                ListPemain(
+                    onUtama = {cancelAndBackToMain(navController)},
+                    onDaftar = {navController.navigate(Navigasi.Form.name)}
+                )
+            }
+            composable(route = Navigasi.Form.name) {
+                FormPendaftaran(
+                    onList = {backToList(navController)},
+                    onSubmit = {},
+                )
+            }
+        }
+    }
+}
+
