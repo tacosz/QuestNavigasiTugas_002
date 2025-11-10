6857eb868e9b62fcba19af4b321824f006b67fb8
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-10 12:14:41 +0700

    membuat tampilan untuk pemilihan gender

--- app/src/main/java/com/example/praktikum6pam/view/daftar.kt
+++ app/src/main/java/com/example/praktikum6pam/view/daftar.kt
@@ -2,13 +2,18 @@ package com.example.praktikum6pam.view
 
 import android.media.Image
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
@@ -16,10 +21,15 @@ import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,6 +38,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -140,6 +152,82 @@ fun FormPendaftaran(
                 fontWeight = FontWeight.Bold,
                 color = Color.LightGray
             )
+            Card (
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.Transparent
+                ),
+                modifier = Modifier
+                    .padding(top = 12.dp, start = 24.dp, end = 24.dp, bottom = 80.dp)
+                    .fillMaxSize()
+                    .clip(shape = RoundedCornerShape(4))
+                    .background(
+                        Brush.radialGradient(
+                        colors = listOf(
+                            Color.White.copy(0.8f),
+                            Color.White.copy(0.4f)
+                        )))
+                    .align(Alignment.CenterHorizontally),
+
+            ) {
+                Column (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp, start = 20.dp, end = 20.dp),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Column {
+                        Text(
+                            text = "Full Name",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        TextField(
+                            value = txtName,
+                            singleLine = true,
+                            onValueChange = {txtName = it},
+                            shape = RoundedCornerShape(12.dp),
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Column {
+                        Text(
+                            text = "Gender",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Row {
+                            jenis.forEach { item ->
+                                Row (modifier = Modifier
+                                    .selectable(
+                                        selected = txtGender == item,
+                                        onClick = {txtGender = item}
+                                    ), verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    RadioButton(
+                                        selected = txtGender == item,
+                                        onClick = {txtGender = item},
+                                        modifier = Modifier
+                                            .width(12.dp)
+                                            .height(12.dp)
+                                            .padding(start = 8.dp)
+                                    )
+                                    Spacer(modifier = Modifier.width(16.dp))
+                                    Text(
+                                        item,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 16.sp
+                                    )
+                                    Spacer(modifier = Modifier.width(20.dp))
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
