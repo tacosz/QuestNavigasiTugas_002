da1a7ea1ad9f2e579b30787e97bc0e409c41aec8
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-10 11:27:58 +0700

    menambahkan background

--- app/src/main/java/com/example/praktikum6pam/view/daftar.kt
+++ app/src/main/java/com/example/praktikum6pam/view/daftar.kt
@@ -1,7 +1,10 @@
 package com.example.praktikum6pam.view
 
+import android.media.Image
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
@@ -14,6 +17,7 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -24,6 +28,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -112,5 +118,15 @@ fun FormPendaftaran(
                 }
             }
         )
+    }
+    val image = painterResource(id = R.drawable.bg)
+    Surface (modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column () {  }
     }
 }
